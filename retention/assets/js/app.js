737c0c4451b3c54a801860b8a69e59facd473f96
Arnaud Masselin <web@adfinitas.fr>
2017-06-14 10:56:03 +0000

    Updated 'retention/assets/js/app.js' via CloudCannon

--- retention/assets/js/app.js
+++ retention/assets/js/app.js
@@ -42,10 +42,8 @@ function verticalAlign() {
 
   if ($(window).width() > 640 && $(window).height() > 850)
     $('.vertical-center').css("margin-top", margin);
-    else if ($(window).height() > 700 && $(window).height() < 850)
-    $('.vertical-center').css("margin-top", "250");
   else
-    $('.vertical-center').css("margin-top", "100");
+    $('.vertical-center').css("margin-top", "250");
 };
 
 $(window).resize( function() {
