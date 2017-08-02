00ab2bee3f47209a4d0901420ee265438aa36513
Arnaud Masselin <web@adfinitas.fr>
2017-08-02 09:41:10 +0000

    Updated 'retention/assets/js/app.js' via CloudCannon

--- retention/assets/js/app.js
+++ retention/assets/js/app.js
@@ -178,7 +178,7 @@ function validateForm() {
   submitForm();
   $.ajax({
   url: 'https://www.mesopinions.com/index.php?f=petition&a=getcounterplus&petition=30770&token=20170625496',
-  type: 'POST'
+  type: 'POST',
   dataType: "jsonp",
   success: function(data) { 
     $('#nbVote').text(data);
